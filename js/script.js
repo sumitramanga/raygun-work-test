d3ffbe05fa5eb3a96602ac4fde314a4c29f3b3f9
sumitramanga <sumitramanga@gmail.com>
2018-12-16 16:44:41 +1300

    Made content switcher more dynamic

--- js/script.js
+++ js/script.js
@@ -3,54 +3,38 @@
 	var contentSwitchBtn1 = document.getElementById('errorCrashesBtn');
 	var contentSwitchBtn2 = document.getElementById('frontEndBtn');
 	var contentSwitchBtn3 = document.getElementById('serverSideBtn');
+	
+	function showContent(e, contentType) {
+		var tabContent = document.getElementsByClassName("contentSwitcherContent");
+		var tabLinks = document.getElementsByClassName("contentSwitcher__item");
 
-	var errorCrashesContent = document.getElementById('errorCrashesContent');
-	var frontEndContent = document.getElementById('frontEndContent');
-	var serverSideContent = document.getElementById('serverSideContent');
-
-
-	function addContentSwitchClasses() {
-		if(this.id === 'errorCrashesBtn') {
-			// Add active class to clicked btn. Remove active class from other btns
-			// and add unactive class
-			contentSwitchBtn1.classList.add('contentSwitcher__item--active');
-			contentSwitchBtn1.classList.remove('contentSwitcher__item--unactive');
-			contentSwitchBtn2.classList.remove('contentSwitcher__item--active');
-			contentSwitchBtn2.classList.add('contentSwitcher__item--unactive');
-			contentSwitchBtn3.classList.remove('contentSwitcher__item--active');
-			contentSwitchBtn3.classList.add('contentSwitcher__item--unactive');
-
-			errorCrashesContent.style.display = 'block';
-			frontEndContent.style.display = 'none';
-			serverSideContent.style.display = 'none';
-
-		} else if(this.id === 'frontEndBtn') {
-			contentSwitchBtn2.classList.add('contentSwitcher__item--active');
-			contentSwitchBtn2.classList.remove('contentSwitcher__item--unactive');
-			contentSwitchBtn1.classList.remove('contentSwitcher__item--active');
-			contentSwitchBtn1.classList.add('contentSwitcher__item--unactive');
-			contentSwitchBtn3.classList.remove('contentSwitcher__item--active');
-			contentSwitchBtn3.classList.add('contentSwitcher__item--unactive');
-
-			errorCrashesContent.style.display = 'none';
-			frontEndContent.style.display = 'block';
-			serverSideContent.style.display = 'none';
-
-		} else if (this.id === 'serverSideBtn') {
-			contentSwitchBtn3.classList.add('contentSwitcher__item--active');
-			contentSwitchBtn3.classList.remove('contentSwitcher__item--unactive');
-			contentSwitchBtn2.classList.remove('contentSwitcher__item--active');
-			contentSwitchBtn2.classList.add('contentSwitcher__item--unactive');
-			contentSwitchBtn1.classList.remove('contentSwitcher__item--active');
-			contentSwitchBtn1.classList.add('contentSwitcher__item--unactive');
-
-			errorCrashesContent.style.display = 'none';
-			frontEndContent.style.display = 'none';
-			serverSideContent.style.display = 'block';
+		for (i = 0; i < tabContent.length; i++) {
+			tabContent[i].style.display = "none";
 		}
+
+		for (i = 0; i < tabLinks.length; i++) {
+			tabLinks[i].className = tabLinks[i].className.replace(" contentSwitcher__item--active", " contentSwitcher__item--unactive");
+		}
+
+		document.getElementById(contentType).style.display = "block";
+		e.currentTarget.className += " contentSwitcher__item--active";
+		e.currentTarget.classList.remove("contentSwitcher__item--unactive");
 	}
-		contentSwitchBtn1.addEventListener('click', addContentSwitchClasses);
-		contentSwitchBtn2.addEventListener('click', addContentSwitchClasses);
-		contentSwitchBtn3.addEventListener('click', addContentSwitchClasses);
+
+	function contentSwitchBtn1Clicked(){
+		 showContent(event, 'errorCrashesContent');
+	}
+
+	function contentSwitchBtn2Clicked(){
+		 showContent(event, 'frontEndContent');
+	}
+
+	function contentSwitchBtn3Clicked(){
+		 showContent(event, 'serverSideContent');
+	}
+
+	contentSwitchBtn1.addEventListener('click', contentSwitchBtn1Clicked);
+	contentSwitchBtn2.addEventListener('click', contentSwitchBtn2Clicked);
+	contentSwitchBtn3.addEventListener('click', contentSwitchBtn3Clicked);
 
 }());
